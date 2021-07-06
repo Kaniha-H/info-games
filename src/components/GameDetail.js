d7e223098ea21d63d34cf647e0d5a3aff1a54503
HENG Kaniha <hengkaniha@gmail.com>
2021-07-06 14:42:36 +0200

    redux trick

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,94 +6,98 @@ import { motion } from 'framer-motion';
 import { useSelector } from 'react-redux';
 
 const GameDetail = () => {
-    // data
-    const { screen, game } = useSelector((state) => state.detail);
-    return(
-        <CardShadow>
+  // data
+  const { screen, game, isLoading } = useSelector((state) => state.detail);
+  return(
+    <>
+      {!isLoading && (
+          <CardShadow>
             <Detail>
                 <Stats>
-                    <div className="rating">
-                        <h3>{ game.name }</h3>
-                        <p>Rating: { game.rating }</p>
-                    </div>
-                    <Info>
-                        <h3>Platforms</h3>
-                        <Platforms>
-                            { game.platforms.map(data => (
-                                <h3 key={data.platform.id}>{ data.platform.name }</h3>
-                            )) }
-                        </Platforms>
-                    </Info>
+                  <div className="rating">
+                    <h3>{ game.name }</h3>
+                    <p>Rating: { game.rating }</p>
+                  </div>
+                  <Info>
+                    <h3>Platforms</h3>
+                    <Platforms>
+                      { game.platforms.map(data => (
+                        <h3 key={data.platform.id}>{ data.platform.name }</h3>
+                      )) }
+                    </Platforms>
+                  </Info>
                 </Stats>
                 <Media>
-                    <img src={game.background_image} alt={game.background_image} />
+                  <img src={game.background_image} alt={game.background_image} />
                 </Media>
                 <Description>
-                    <p>{ game.description_raw }</p>
+                  <p>{ game.description_raw }</p>
                 </Description>
                 <div className="gallery">
-                    {screen.results.map(screen => (
-                        <img src={screen.image} key={screen.id} alt={screen.image} />
-                    ))}
+                  {screen.results.map(screen => (
+                    <img src={screen.image} key={screen.id} alt={screen.image} />
+                  ))}
                 </div>
             </Detail>
-        </CardShadow>
-    );
+          </CardShadow>
+        )}
+    </>
+  );
 };
 
 const CardShadow = styled(motion.div)`
-    width: 100%;
-    min-height: 100vh;
-    overflow-y: scroll;
-    background: rgba(0, 0, 0, .5);
-    position: fixed;
-    top: 0;
-    left: 0;
-    &::-webkit-scrollbar {
-        width: .5rem;
-    }
-    &::-webkit-scrollbar-thumb {
-        background-color: #ff7676;
-    }
-    &::-webkit-scrollbar-track {
-        background: #fff;
-    }
+  width: 100%;
+  min-height: 100vh;
+  overflow-y: scroll;
+  background: rgba(0, 0, 0, .5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  &::-webkit-scrollbar {
+    width: .5rem;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #ff7676;
+  }
+  &::-webkit-scrollbar-track {
+    background: #fff;
+  }
 `;
 const Detail = styled(motion.div)`
-    width: 80%;
-    border-radius: 1rem;
-    padding: 2rem 5rem;
-    background: #fff;
-    position: absolute;
-    left: 10%;
-    color: #000;
-    img {
-        width: 100%;
-    }
+  width: 80%;
+  border-radius: 1rem;
+  padding: 2rem 5rem;
+  background: #fff;
+  position: absolute;
+  left: 10%;
+  color: #000;
+  img {
+    width: 100%;
+  }
 `;
 const Stats = styled(motion.div)`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 const Info = styled(motion.div)`
-    text-align: center;
+  text-align: center;
 `;
 const Platforms = styled(motion.div)`
-    display: flex;
-    justify-content: space-evenly;
-    img {
-        margin-left: 3rem;
-    }
+  display: flex;
+  justify-content: space-evenly;
+  img {
+    margin-left: 3rem;
+  }
 `;
 const Media = styled(motion.div)`
-    margin-top: 5rem;
-    img {
-        width: 100%;
-    }
+  margin-top: 5rem;
+  img {
+    width: 100%;
+  }
 `;
 const Description = styled(motion.div)`
-    margin: 5rem 0;
+  margin: 5rem 0;
 `;
 
 export default GameDetail;
